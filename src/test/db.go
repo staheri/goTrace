4c20f09ff50fa2c1daa0f8b3ff0a04f4384e2190
Saeed Taheri <saeed@Saeeds-MacBook-Pro-3.local>
2020-05-20 18:21:49 -0600

    added script to create global event category tables

--- src/test/db.go
+++ src/test/db.go
@@ -8,6 +8,7 @@ import (
 	_"path"
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
+	"log"
 
 
 )
@@ -21,36 +22,76 @@ func main(){
 		fmt.Println("Connection Established")
 	}
 
-	/*name := "test1"
+	name := "test1"
 
-	_,err = db.Exec("CREATE DATABASE "+name)
-   if err != nil {
-       panic(err)
-   }
+	_,err = db.Exec("CREATE DATABASE IF NOT EXISTS "+name)
+	if err != nil {
+			panic(err)
+	}
 
+	fmt.Println("Database Created")
+
+	sqlStatement := "USE "+name+";"
+	res,err := db.Exec(sqlStatement)
+	if err == nil {
+ 		fmt.Printf("%v\n",res)
+		lastId, err1 := res.LastInsertId()
+		if err1 != nil {
+			log.Fatal(err1)
+		}
+		rowCnt, err1 := res.RowsAffected()
+		if err1 != nil {
+			log.Fatal(err1)
+		}
+		log.Printf("ID = %d, affected = %d\n", lastId, rowCnt)
+ 	}else{
+ 		fmt.Println("ERRRRRR")
+ 		panic(err)
+ 	}
+
+	sqlStatement = `SHOW TABLES;`
+	res,err = db.Exec(sqlStatement)
+	if err == nil {
+ 		fmt.Printf("%v\n",res)
+		lastId, err1 := res.LastInsertId()
+		if err1 != nil {
+			log.Fatal(err1)
+		}
+		rowCnt, err1 := res.RowsAffected()
+		if err1 != nil {
+			log.Fatal(err1)
+		}
+		log.Printf("ID = %d, affected = %d\n", lastId, rowCnt)
+ 	}else{
+ 		fmt.Println("ERRRRRR")
+ 		panic(err)
+ 	}
+
+
+	 /*
    _,err = db.Exec("USE "+name)
    if err != nil {
        panic(err)
    }
 
-   _,err = db.Exec("CREATE TABLE example ( id integer, data varchar(32) )")
+  /* _,err = db.Exec("CREATE TABLE example ( id integer, data varchar(32) )")
    if err != nil {
        panic(err)
    }
 	 */
 
-	 sqlStatement := `SHOW TABLES`
-	 res,err := db.Exec(sqlStatement)
+	// sqlStatement := `SHOW TABLES`
+	 //res,err := db.Exec(sqlStatement)
  	//var table string
- 	if err != nil {
+ 	//if err != nil {
  		/*for res.Next() {
  			res.Scan(&table)
  			fmt.Println(table)
  		}*/
- 		fmt.Printf("%v",res)
- 	}else{
- 		fmt.Println("ERRRRRR")
- 		panic(err)
- 	}
+ 		//fmt.Printf("%v",res)
+ 	//}else{
+ 		//fmt.Println("ERRRRRR")
+ 		//panic(err)
+ 	//}
 
 }
