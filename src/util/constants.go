ee4879d4d14bcb62b81ea626e4bc49c36796e4dc
Saeed Taheri <staheri@cs.utah.edu>
2020-07-16 11:09:15 -0600

    added topx constant

--- src/util/constants.go
+++ src/util/constants.go
@@ -1,9 +1,11 @@
 package util
-// 
+//
 // const HOME = "/Users/saeed/goTrace"
 // const HACPATH = HOME + "/scripts/hac"
 // const CLPATH = HOME + "/cl"
 // const RESPATH = HOME + "/results"
+
+const TOPX = 500
 
 // Event types in the trace.
 // Verbatim copy from src/runtime/trace.go with the "trace" prefix removed.
