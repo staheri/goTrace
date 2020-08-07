73a29b89913ce9ca07ab66e1032c610d42c64a2e
Saeed Taheri <staheri@cs.utah.edu>
2020-08-07 13:21:28 -0600

    output file name fixed

--- src/db/diff.go
+++ src/db/diff.go
@@ -197,9 +197,9 @@ func DIFF(dbName, baseDBName, cloutpath,resultpath string, consec, rid int, aspe
 	}
 
 	// Execute python hac on cloutdir/cl
-	_cmd = "python "+ HACPATH + "/main.py " + cloutdir+"/cl/diff_"+dbName+"_"+filts+optionsDirName+".dot "+resultpath+"/"+dbName+"_"+filts
+	_cmd = "python "+ HACPATH + "/main.py " + cloutdir+"/cl/diff_"+dbName+"_"+filts+optionsDirName+".dot "+resultpath+"/"+dbName+"_"+filts+optionsDirName
 
-	cmd = exec.Command("python",HACPATH + "/main.py",cloutdir+"/cl/diff_"+dbName+"_"+filts+optionsDirName+".dot",resultpath+"/"+dbName+"_"+filts)
+	cmd = exec.Command("python",HACPATH + "/main.py",cloutdir+"/cl/diff_"+dbName+"_"+filts+optionsDirName+".dot",resultpath+"/"+dbName+"_"+filts+optionsDirName)
 	fmt.Printf(">>> Executing %s...\n",_cmd)
 	//err = cmd.Run()
 	var out bytes.Buffer
