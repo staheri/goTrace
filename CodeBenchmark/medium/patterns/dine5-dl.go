944ce378de7e4f8aae1166430243f58568c8f45b
Saeed Taheri <staheri@cs.utah.edu>
2020-08-07 13:10:20 -0600

    modified benchmarks

--- CodeBenchmark/medium/patterns/dine5-dl.go
+++ CodeBenchmark/medium/patterns/dine5-dl.go
@@ -3,22 +3,30 @@
 package main
 
 import (
-	"fmt"
+	_"fmt"
 	"sync"
 	"time"
 )
 
 
+const bound = 10
+
 func phil(m1, m2 *sync.Mutex, f1, f2 *int, id int) {
-	for {
+	eats := 0
+	//for i := 0 ; i<bound ; i++{
+	for{
 		m1.Lock()
-		fmt.Printf("Phil %d got left fork", id)
+		//fmt.Printf("Phil %d got left fork\n", id)
 		*f1 += 1
 		m2.Lock()
-		fmt.Printf("Phil %d right fork", id)
+		//fmt.Printf("Phil %d right fork\n", id)
 		*f2 += 1
+		eats += 1
 		m2.Unlock()
 		m1.Unlock()
+		if eats > bound{
+			break
+		}
 	}
 }
 
@@ -33,6 +41,6 @@ func main() {
 	go phil(m2, m3, &f2, &f3, 1)
 	go phil(m3, m4, &f3, &f4, 2)
 	go phil(m4, m5, &f4, &f5, 3)
-	go phil(m1, m5, &f1, &f5, 4)
-	time.Sleep(10*time.Second)
+	go phil(m5, m1, &f5, &f1, 4)
+	time.Sleep(1*time.Second)
 }
