aa976088a5d8253eb0123e8943924717cb1f195a
Saeed Taheri <staheri@cs.utah.edu>
2020-08-31 17:52:34 -0600

    worker scripts added

---
+++ scripts/worker.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+./src -app=../CodeBenchmark/temp/fft1.go -cmd=hac -outdir=../results/ -cons=2 -atrmode=2 ;
+./src -app=../CodeBenchmark/temp/fft1.go -cmd=hac -outdir=../results/ -cons=3 -atrmode=2 ;
+./src -app=../CodeBenchmark/temp/fft1.go -cmd=hac -outdir=../results/ -cons=4 -atrmode=2 ;
+./src -app=../CodeBenchmark/temp/fft1.go -cmd=hac -outdir=../results/ -cons=5 -atrmode=2 ;
+./src -app=../CodeBenchmark/temp/fft1.go -cmd=hac -outdir=../results/ -cons=6 -atrmode=2 ;
+./src -app=../CodeBenchmark/temp/fft1.go -cmd=hac -outdir=../results/ -cons=7 -atrmode=2 ;
+./src -app=../CodeBenchmark/temp/fft1.go -cmd=hac -outdir=../results/ -cons=8 -atrmode=2 ;
+
+./src -app=../CodeBenchmark/temp/fft1.go -cmd=hac -outdir=../results/ -cons=2 -atrmode=3 ;
+./src -app=../CodeBenchmark/temp/fft1.go -cmd=hac -outdir=../results/ -cons=3 -atrmode=3 ;
+./src -app=../CodeBenchmark/temp/fft1.go -cmd=hac -outdir=../results/ -cons=4 -atrmode=3 ;
+./src -app=../CodeBenchmark/temp/fft1.go -cmd=hac -outdir=../results/ -cons=5 -atrmode=3 ;
+./src -app=../CodeBenchmark/temp/fft1.go -cmd=hac -outdir=../results/ -cons=6 -atrmode=3 ;
+./src -app=../CodeBenchmark/temp/fft1.go -cmd=hac -outdir=../results/ -cons=7 -atrmode=3 ;
+./src -app=../CodeBenchmark/temp/fft1.go -cmd=hac -outdir=../results/ -cons=8 -atrmode=3 ;
+
+./src -app=../CodeBenchmark/temp/fft1.go -cmd=hac -outdir=../results/ -cons=2 -atrmode=4 ;
+./src -app=../CodeBenchmark/temp/fft1.go -cmd=hac -outdir=../results/ -cons=3 -atrmode=4 ;
+./src -app=../CodeBenchmark/temp/fft1.go -cmd=hac -outdir=../results/ -cons=4 -atrmode=4 ;
+./src -app=../CodeBenchmark/temp/fft1.go -cmd=hac -outdir=../results/ -cons=5 -atrmode=4 ;
+./src -app=../CodeBenchmark/temp/fft1.go -cmd=hac -outdir=../results/ -cons=6 -atrmode=4 ;
+./src -app=../CodeBenchmark/temp/fft1.go -cmd=hac -outdir=../results/ -cons=7 -atrmode=4 ;
+./src -app=../CodeBenchmark/temp/fft1.go -cmd=hac -outdir=../results/ -cons=8 -atrmode=4 ;
---
+++ src/worker.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+APP=../CodeBenchmark/temp/fft1.go
+CMD=rr
+OUT=../results
+SRC=native
+#CAT=CHNL\ GRTN\ GRTN,CHNL\ WGRP\ CHNL,WGRP\ GRTN,CHNL,WGRP\ GRTN,WGRP\ MUTX\ GCMM
+
+for S in 4 8 16 32 64 128 256 512 1024 2048
+do
+  echo $SIZE_FFT;
+  export SIZE_FFT=$S
+  ./src -app=$APP -cmd=$CMD -outdir=$OUT -src=$SRC CHNL;
+done
+
+#for C in 2 3 4 5 6 7 8
+#do
+#  for A in 2 3 4
+#  do
+#    ./src -app=$APP -cmd=$CMD -outdir=$OUT -src=$SRC -cons=$C -atrmode=$A $CAT ;
+#  done
+#done
