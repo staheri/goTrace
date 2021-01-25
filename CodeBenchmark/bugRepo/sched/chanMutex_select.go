1f81b70ae48dde1a6e490ba75aac432ca4d639f0
Saeed Taheri <staheri@cs.utah.edu>
2021-01-25 04:13:33 -0700

    no change

--- CodeBenchmark/bugRepo/sched/chanMutex_select.go
+++ CodeBenchmark/bugRepo/sched/chanMutex_select.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-  "fmt"
-  "sync"
-  "runtime"
-  "time"
+	"fmt"
+	"sync"
+	"runtime"
+	"time"
 )
 
 // commit hash: a69a59ffc7e3d028a72d1195c2c1535f447eaa84
@@ -13,45 +13,45 @@ import (
 // Different timing in select options and conditionals cause deadlock
 
 func main() {
-  runtime.GOMAXPROCS(4)
-  cv := sync.NewCond(&sync.Mutex{})
 
-  done := make(chan int)
-  eventch := make(chan int)
-  closec := make(chan int)
+	runtime.GOMAXPROCS(4)
+	cv := sync.NewCond(&sync.Mutex{})
 
-  go g0(closec)
-  go g1(cv,done,eventch,closec)
-  go g2(cv,done,eventch)
+	done := make(chan int)
+	eventch := make(chan int)
+	closec := make(chan int)
 
-  time.Sleep(time.Millisecond*10)
-  fmt.Println("End of main!")
+	go g0(closec)
+	go g1(cv, done, eventch, closec)
+	go g2(cv, done, eventch)
+
+	time.Sleep(time.Millisecond * 10)
+	fmt.Println("End of main!")
 }
 
-
-func g0(ch chan int){
-  //time.Sleep(time.Millisecond*10)
+func g0(ch chan int) {
+	//time.Sleep(time.Millisecond*10)
   //runtime.Gosched()
   // to trigger the bug, it has to happen first
-  close(ch)
+	close(ch)
 }
 
-func g1(cv *sync.Cond, done,eventch,closec chan int){
-  select{
-  case <- eventch:
-  case <- closec:
-    cv.L.Lock()
-    cv.Wait()
-    cv.L.Unlock()
-    close(done)
-  }
+func g1(cv *sync.Cond, done, eventch, closec chan int) {
+	select {
+	case <-eventch:
+	case <-closec:
+		cv.L.Lock()
+		cv.Wait()
+		cv.L.Unlock()
+		close(done)
+	}
 }
 
-func g2(cv *sync.Cond, done,eventch chan int){
-  runtime.Gosched()
-  select {
-  case eventch <- 1:
-  case <- done:
-  }
-  cv.Broadcast()
+func g2(cv *sync.Cond, done, eventch chan int) {
+	runtime.Gosched()
+	select {
+	case eventch <- 1:
+	case <-done:
+	}
+	cv.Broadcast()
 }
