75534de0cda6468c7770facee74610cdd173d300
Saeed Taheri <staheri@cs.utah.edu>
2021-02-08 15:23:28 -0700

    cleaned and logged

--- src/schedtest/schedtest.go
+++ src/schedtest/schedtest.go
@@ -19,6 +19,7 @@ import (
 func SchedTest(app,src,x string, to,depth,iter int) *instrument.AppTest {
 
 	// execute the base first
+	fmt.Println("SchedTest: Execute base run...")
 	base := instrument.NewAppExec(app,src,x,to)
 	dbn ,err := base.DBPointer()
 	base.DBName = dbn
@@ -26,23 +27,20 @@ func SchedTest(app,src,x string, to,depth,iter int) *instrument.AppTest {
 		panic(err)
 	}
 
+	fmt.Println("SchedTest: Initialize new test run...")
 	test := instrument.NewAppTest(base,depth)
 	// obtain the base rewritten version
 	test.OrigPath = strings.Split(base.OrigPath,".go")[0]+"_mod.go"
 
-
 	// create a dir to store rewritten schedtest version permanently
 	temp := filepath.Dir(test.OrigPath)+"/"+base.App
-	//err = os.Mkdir(temp,0700)
-	//if err != nil{
-		//panic(err)
-	//}
 	test.TestPath = temp+"/"+test.Name
 	err = os.MkdirAll(test.TestPath,os.ModePerm)
 	if err != nil{
 		panic(err)
 	}
 
+	fmt.Println("SchedTest: Rewrites permanent schedTest version: ",test.TestPath)
 	// rewrite the schedTest based on the base rewritten and concusage
 	err = test.RewriteSourceSched()
 	if err != nil{
@@ -53,6 +51,7 @@ func SchedTest(app,src,x string, to,depth,iter int) *instrument.AppTest {
 	//    trace (execute,collect,store) the permanent version schedTest
 	//    executeTrace(app.NewPath)
 	//    add dbnames to test object
+	fmt.Println("SchedTest: Testing iterations begin...")
 	for i := 0 ; i<iter ; i++ {
 		events, err := instrument.ExecuteTrace(test.TestPath)
 		if err != nil{
@@ -60,7 +59,8 @@ func SchedTest(app,src,x string, to,depth,iter int) *instrument.AppTest {
 			return nil
 		}
 		dbn := db.Store(events,test.Name)
-		db.Checker(dbn)
+		fmt.Printf("Test run %d/%d (%s):\n",i+1,iter,dbn)
+		db.Checker(dbn,false)
 		test.DBNames[i] = dbn
 	}
 	return test
