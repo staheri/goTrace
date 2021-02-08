442da3caa555671cc3b2b437172d0ab6d6fc79e3
Saeed Taheri <staheri@cs.utah.edu>
2021-02-07 23:34:08 -0700

    long/short report

--- src/db/checker.go
+++ src/db/checker.go
@@ -15,7 +15,7 @@ import (
 	_"text/tabwriter"
 )
 
-func Checker(dbName string) {
+func longLeakReport(dbName string) {
 	// Establish connection to DB
 	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/"+dbName)
 	if err != nil {
@@ -158,128 +158,96 @@ func textReport(lastEvents map[int]string){
 		fmt.Println("Leaked Goroutines:",string(colorGreen),"NONE",string(colorReset))
 	}
 }
-/*func ResourceGraph(dbName, outdir string){
 
-	// Variables
-	var q1,q2, event             string
-	var rid          string
-	var  g     int
-	var pos int
+func Checker(dbName string, long bool){
+	if long {
+		longLeakReport(dbName)
+	} else{
+		shortLeakReport(dbName)
+	}
+}
 
-	edges := make(map[string][]string)
-	waitingEdges := make(map[string][]string)
-	nodes := make(map[string]int)
+func shortLeakReport(dbName string) {
+	// Establish connection to DB
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/"+dbName)
+	if err != nil {
+		fmt.Println(err)
+	}else{
+		fmt.Println("Connection Established")
+	}
+	defer db.Close()
+	// END DB
 
+	//fmt.Println(appGoroutineFinder(db))
+	var gs []int
+	var g int
+	var event string
 
+	// last events stroe every last event of goroutines
+	lastEvents := make(map[int]string)
 
-
-	res, err = db.Query(q2)
+	// Query mutexes
+	q := `select gid from goroutines;`
+	res, err := db.Query(q)
 	if err != nil {
 		panic(err)
 	}
 	for res.Next(){
-		err = res.Scan(&event,&g,&rid,&pos)
-		if err != nil{
-			panic(err)
-		}
-		if rid != "M3"{ // trace lock, ignore it
-			if event == "EvChSend"{
-				if pos != 0{
-					edges["G"+strconv.Itoa(g)] = append(edges["G"+strconv.Itoa(g)],rid)
-				}else{
-					waitingEdges["G"+strconv.Itoa(g)] = append(waitingEdges["G"+strconv.Itoa(g)],rid)
-				}
-			} else{
-				if pos != 0{
-					edges[rid] = append(edges[rid],"G"+strconv.Itoa(g))
-				}else{
-					waitingEdges[rid] = append(waitingEdges[rid],"G"+strconv.Itoa(g))
-				}
-			}
-			if _,ok := nodes["G"+strconv.Itoa(g)] ; !ok{
-				nodes["G"+strconv.Itoa(g)] = 1
-			}
-			if _,ok := nodes[rid] ; !ok{
-				nodes[rid] = 1
-			}
-		}
-
+		err = res.Scan(&g)
+		check(err)
+		gs = append(gs,g) // append g to gs
 	}
 	res.Close()
 
-
-	nodes_st := ""
-	for k,_ := range nodes{
-		if strings.HasPrefix(k,"G"){
-			nodes_st = nodes_st + k + " [label = \""+k+"\" shape=circle]\n\t"
-		}else if strings.HasPrefix(k,"C"){
-			nodes_st = nodes_st + k + " [label = \""+k+"\" shape=diamond style=bold]\n\t"
-		} else{
-			nodes_st = nodes_st + k + " [label = \""+k+"\" shape=invtriangle style=bold]\n\t"
-		}
-	}
-
-	edges_st := ""
-	for k,v := range edges{
-		freq := make(map[string]int)
-		for _,d := range v{
-			freq[d]++
-		}
-		for kk,vv := range freq{
-			edges_st = edges_st + k + " -> " + kk + " [label=\""+strconv.Itoa(vv)+"\"]\n\t"
-		}
-	}
-
-	edges_st = edges_st + "\n\n"
-
-	for k,v := range waitingEdges{
-		freq := make(map[string]int)
-		for _,d := range v{
-			freq[d]++
-		}
-		for kk,vv := range freq{
-			edges_st = edges_st + k + " -> " + kk + " [label=\""+strconv.Itoa(vv)+"\" style=dashed]\n\t"
-		}
-	}
-
-	fmt.Println(nodes_st)
-	fmt.Println(edges_st)
-
-
-	f, err := os.Create(outdir+"/"+dbName+"_resg.dot")
+	lastEventStmt,err := db.Prepare("SELECT type FROM Events WHERE g=? ORDER BY id DESC LIMIT 1")
 	check(err)
-	f.WriteString("digraph {\n\t"+nodes_st+"\n\n\t"+edges_st+"\n}")
-	f.Close()
+	defer lastEventStmt.Close()
 
-	// Create pdf
-	_cmd := "dot -Tpdf "+ outdir+"/"+dbName+"_resg.dot" + " -o " + outdir+"/"+dbName+"_resg.pdf"
-
-	cmd := exec.Command("dot","-Tpdf",outdir+"/"+dbName+"_resg.dot","-o",outdir+"/"+dbName+"_resg.pdf")
-	fmt.Printf(">>> Executing %s...\n",_cmd)
-	//err = cmd.Run()
-	var stdout bytes.Buffer
-	var stderr bytes.Buffer
-	cmd.Stdout = &stdout
-	cmd.Stderr = &stderr
-	err = cmd.Run()
-	if err != nil {
-    fmt.Println(fmt.Sprint(err) + ": " + stderr.String())
-    return
+	for _,gi := range(gs){
+		// Last event
+		res,err = lastEventStmt.Query(gi)
+		check(err)
+		for res.Next(){
+			err = res.Scan(&event)
+			check(err)
+			lastEvents[gi]=event
+		}
+		res.Close()
 	}
-	fmt.Println("Result: " + stdout.String())
+
+	// ****************
+
+	var suspicious []int
+	var   isGlobalDL   bool
+	var   numIgnore   int
+
+	colorReset := "\033[0m"
+	colorRed := "\033[31m"
+	colorGreen := "\033[32m"
 
 
-	_cmd = "open" + outdir+"/"+dbName+"_resg.pdf"
-
-	cmd = exec.Command("open",outdir+"/"+dbName+"_resg.pdf")
-	fmt.Printf(">>> Executing %s...\n",_cmd)
-	cmd.Stdout = &stdout
-	cmd.Stderr = &stderr
-	err = cmd.Run()
-	if err != nil {
-    fmt.Println(fmt.Sprint(err) + ": " + stderr.String())
-    return
+	for k,v := range(lastEvents){
+		if k == 1 && v != "EvGoSched"{
+			isGlobalDL = true
+			continue
+		}
+		switch v {
+		case "EvGoWaiting","EvGoEnd","EvGoBlock":
+			numIgnore++
+		default:
+			if k != 0 && k != 1{
+				// the goroutine is in app goroutine which has not ended!
+				suspicious = append(suspicious,k)
+			}
+		}
 	}
-	fmt.Println("Result: " + stdout.String())
-	//fmt.Println(out)
-}*/
+
+	if isGlobalDL{
+		fmt.Println(string(colorRed),"Fail (global deadlock)",string(colorReset))
+	} else if len(suspicious) != 0{
+		fmt.Println(string(colorRed),"Fail (partial deadlock - leak)",string(colorReset))
+	} else{
+		fmt.Println(string(colorGreen),"Pass",string(colorReset))
+	}
+
+}
